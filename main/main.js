642c9e00a764463fa60ae0fa9c2cfc60678fc0ea
Daniel <danilagalakov3@gmail.com>
2022-12-11 22:06:03 +0600

    еуые

--- main/main.js
+++ main/main.js
@@ -1,133 +1,7 @@
 'use strict';
-// let counterClickHome = 0;
-// document.querySelector("#type-home").addEventListener("click",()=>{
-//     counterClickCode = 0;
-//     counterClickCacpcha = 0;
-//     counterClickSymbol = 0;
-//     counterClickText = 0;
-//     counterClickHome++;
-//     if(counterClickHome > 1) {
-//         return false;
-//     }
-//     fetch('main/home.html').then((response) => {
-//         return response.text()
-//     }).then((body) => {
-//         var parser = new DOMParser();
-//         var doc = parser.parseFromString(body, "text/html");
-//         document.querySelector("main").innerHTML = new XMLSerializer().serializeToString(doc);
-//     }).catch((err) => {  
-//         console.log('Failed to fetch page: ', err);
-//     });
-// });
-
-// let counterClickText = 0;
-// document.querySelector("#type-text").addEventListener("click",()=>{
-//     counterClickCode = 0;
-//     counterClickCacpcha = 0;
-//     counterClickSymbol = 0;
-//     counterClickHome = 0;
-//     counterClickText++;
-//     if(counterClickText > 1) {
-//         return false;
-//     }
-//     fetch('text/setting.html').then((response) => {
-//         return response.text();
-//     }).then((body) => {
-//         var parser = new DOMParser();
-//         var doc = parser.parseFromString(body, "text/html");
-//         document.querySelector("main").innerHTML = new XMLSerializer().serializeToString(doc);
-//     }).catch((err) => {  
-//         console.log('Failed to fetch page: ', err);
-//     });
-// });
-
-
-// let counterClickCode = 0;
-// document.querySelector("#type-code").addEventListener("click",()=>{
-//     counterClickText = 0;
-//     counterClickCacpcha = 0;
-//     counterClickSymbol = 0;
-//     counterClickHome = 0;
-//     counterClickCode++;
-//     if(counterClickCode > 1) {
-//         return false;
-//     }
-//     fetch('code/code.html').then((response) => {
-//         return response.text();
-//     }).then((body) => {
-//         var parser = new DOMParser();
-//         var doc = parser.parseFromString(body, "text/html");
-//         document.querySelector("main").innerHTML = new XMLSerializer().serializeToString(doc);
-//     }).catch((err) => {  
-//         console.log('Failed to fetch page: ', err);
-//     });
-// });
-
-
-// let counterClickCacpcha = 0;
-// document.querySelector("#type-capcha").addEventListener("click",()=>{
-//     counterClickText = 0;
-//     counterClickCode = 0;
-//     counterClickSymbol = 0;
-//     counterClickHome = 0;
-//     counterClickCacpcha++;
-//     if(counterClickCacpcha > 1) {
-//         return false;
-//     }
-//     fetch('capcha/setting.html').then((response) => {
-//         return response.text();
-//     }).then((body) => {
-//         var parser = new DOMParser();
-//         var doc = parser.parseFromString(body, "text/html");
-//         document.querySelector("main").innerHTML = new XMLSerializer().serializeToString(doc);
-//     }).catch((err) => {  
-//         console.log('Failed to fetch page: ', err);
-//     });
-// });
-
-// let counterClickSymbol = 0;
-// document.querySelector("#type-symbol").addEventListener("click",()=>{
-//     counterClickText = 0;
-//     counterClickCode = 0;
-//     counterClickCacpcha = 0;
-//     counterClickHome = 0;
-//     counterClickSymbol++;
-//     if(counterClickSymbol > 1) {
-//         return false;
-//     }
-//     fetch('symbol/setting.html').then((response) => {
-//         return response.text();
-//     }).then((body) => {
-//         var parser = new DOMParser();
-//         var doc = parser.parseFromString(body, "text/html");
-//         document.querySelector("main").innerHTML = new XMLSerializer().serializeToString(doc);
-//     }).catch((err) => {  
-//         console.log('Failed to fetch page: ', err);
-//     });
-// });
-
-
-// let count = 0;
-// $(()=>{
-//     $('#type-text').on( ()=>{
-//         count++
-//         if(count > 1) {
-//             return false
-//         }
-//         $.getScript('text/setting.js')
-//     })
-// })
-
-
-// <![CDATA[
-
-// insert teh codez
-
-
-
 $(document).ready(()=>{
     function consWait() {
-        console.log('wait')
+        console.log('wait');
     }
     const ua = window.navigator.userAgent;
     if (ua.indexOf("MSIE ") > 0 || ua.indexOf("Trident") > 0) {
@@ -145,33 +19,31 @@ $(document).ready(()=>{
             datatype : "application/html",
             contentType: "text/html",
             // cache: false,
-            // beforeSend: consWait,
+            beforeSend: consWait,
             success: function(data){
-                console.log(url)
+                console.log(url);
                 $('main').html(data);
             },
             catch: function(e){
-                console.log(e)
+                console.log(e);
             }
-        })
+        });
          $.getScript(script);
-    }
+    };
     
     $('.type-home').click(()=>{
-        urlAjax('/main/home.html')
-    })
+        urlAjax('./main/home.html');
+    });
     $('.type-text').click(()=>{
-        urlAjax('/text/setting.html', 'text/setting.js')
-    })
+        urlAjax('./text/setting.html', 'text/setting.js');
+    });
     $('.type-code').click(()=>{
-        urlAjax('/code/setting.html', 'code/code.js')
-    })
+        urlAjax('./code/setting.html', 'code/code.js');
+    });
     $('.type-capcha').click(()=>{
-        urlAjax('/capcha/capcha.html', 'capcha/capcha.js')
-    })
+        urlAjax('./capcha/capcha.html', 'capcha/capcha.js');
+    });
     $('.type-symbol').click(()=>{
-        urlAjax('/symbol/setting.html', 'symbol/symbol.js')
-    })
-})
-
-// ]]>
+        urlAjax('./symbol/setting.html', 'symbol/symbol.js');
+    });
+});
