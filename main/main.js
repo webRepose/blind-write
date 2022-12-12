f6fdc875b5d13525db0d5ce103ecd7735f918140
Daniel <danilagalakov3@gmail.com>
2022-12-12 17:24:39 +0600

    еуые

--- main/main.js
+++ main/main.js
@@ -16,7 +16,7 @@ $(document).ready(()=>{
         const urlPath = document.location.pathname;
         console.log(urlPath)
         if(urlPath === '/') {
-            urlAjaxNoScript('/blind-write' + urlPath + '/main.html');
+            urlAjaxNoScript('.html');
         } else {
             urlAjaxNoScript('/blind-write' + urlPath + '/main.html');
         }
